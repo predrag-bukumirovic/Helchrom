868de7e0c5b63f0ec2d34a906b3bd1dde8c5b417
Predrag-web <webpredrag@gmail.com>
2023-06-01 18:56:51 +0200

    izmene

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,6 +35,7 @@ export default function Navbar() {
         pathname: "/search",
         search: `?${createSearchParams({ q: search })}`
       });
+      document.activeElement.blur();
     }
   };
 
