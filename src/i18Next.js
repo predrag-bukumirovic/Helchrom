320c1d548466625b1f8be881f4fd5c718a17f0d0
Predrag-web <webpredrag@gmail.com>
2024-07-18 20:27:21 +0200

    fix

--- src/i18Next.js
+++ src/i18Next.js
@@ -1,14 +1,18 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import texts from './texts'
+import enTranslation from './languages/en'
+import geTranslation from "./languages/ge";
 
 i18next.use(initReactI18next).init({
   resources: {
-    en: texts.en,
-    ge: texts.ge,
-    ru: texts.ru
+    en: {
+      translation: enTranslation
+    },
+    ge: {
+      translation: geTranslation
+    }
   },
-  lng: localStorage.getItem("lng") || "en"
+  lng: localStorage.getItem("lng") || "en",
 });
 
 export default i18next;
